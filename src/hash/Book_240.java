be0ac771fcca5a00e59f95c0fab93d0b06e6eb66
HyoeunYoo <yuuhe0913@gmail.com>
2025-05-01 19:46:47 +0900

    add [코테책] p.240 08_4 몸풀기 문제

---
+++ src/hash/Book_240.java
@@ -0,0 +1,34 @@
+/*
+ * [코테책] p.240 08_4 몸풀기 문제
+ * 2025.05.01
+ * [X] 자료구조 / 해시
+ * */
+
+package hash;
+
+import java.util.HashSet;
+
+public class Book_240 {
+    public static boolean solution(int[] arr, int target) {
+        // 키만 저장하는 HashSet
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int i : arr) {
+            // target 에서 현재 원소를 뺀 값이 해시셋에 있는지 확인
+            if (hashSet.contains(target - i)) {
+                return true;
+            }
+            // 없으면 해시셋에 현재 값 저장
+            hashSet.add(i);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 8};
+        int target = 6;
+        boolean result = solution(arr, target);
+        System.out.println(result);
+    }
+}
